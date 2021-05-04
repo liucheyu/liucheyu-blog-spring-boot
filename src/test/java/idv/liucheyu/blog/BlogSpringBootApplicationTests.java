61a58559b2404535de9793a3bb08d98111d9a44d
tetsu <liucheyu1987@gmail.com>
2021-05-04 16:52:01 +0800

    first commit

---
+++ src/test/java/idv/liucheyu/blog/BlogSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package idv.liucheyu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
